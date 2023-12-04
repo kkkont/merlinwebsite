bfa6c6f61ba90d19db647f9362652d7ee8301b9e
kkkont <116272343+kkkont@users.noreply.github.com>
2023-12-04 22:19:46 +0200

    Working on a slideshow on home page

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,28 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+window.onload = function() {
+  const myInterval=setInterval(function() {
+    plusDivs(1);
+  }, 5000);
+ }
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+ 
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+ var slideIndex = 1;
+  showDivs(slideIndex);
+ 
+  function plusDivs(n) {
+    slideIndex += n;
+    showDivs(slideIndex);
   }
-  x[slideIndex-1].style.display = "block";  
-}
+  
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+  }
+
+ 
