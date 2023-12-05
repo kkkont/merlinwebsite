314fa1af5bac20eaebdd6d19f68e0482cd0c28e4
kkkont <116272343+kkkont@users.noreply.github.com>
2023-12-05 10:38:29 +0200

    Finishing slideshow

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,30 @@
-window.onload = function() {
-  const myInterval=setInterval(function() {
-    plusDivs(1);
-  }, 5000);
+var slideIndex = 1;
+var myInterval;
+
+function startSlideShow() {
+ myInterval = setInterval(function() {
+   plusDivs(1);
+ }, 5000);
+}
+
+function plusDivs(n) {
+ clearInterval(myInterval); // Clear the interval
+ slideIndex += n;
+ showDivs(slideIndex);
+ startSlideShow(); // Restart the interval
+}
+
+function showDivs(n) {
+ var i;
+ var x = document.getElementsByClassName("mySlides");
+ if (n > x.length) {slideIndex = 1}
+ if (n < 1) {slideIndex = x.length}
+ for (i = 0; i < x.length; i++) {
+   x[i].style.display = "none"; 
  }
+ x[slideIndex-1].style.display = "block"; 
+}
 
- 
-
- var slideIndex = 1;
-  showDivs(slideIndex);
- 
-  function plusDivs(n) {
-    slideIndex += n;
-    showDivs(slideIndex);
-  }
-  
-  function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    x[slideIndex-1].style.display = "block"; 
-  }
+startSlideShow(); 
 
  
